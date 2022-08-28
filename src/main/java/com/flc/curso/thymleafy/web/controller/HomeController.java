442c985561b3705a2265689284d7dac4641193d8
fernando.correa <paracorrea@gmail.com>
2022-08-28 18:10:37 -0300

    Controllers e domain iniciados

--- src/main/java/com/flc/curso/thymleafy/web/controller/HomeController.java
+++ src/main/java/com/flc/curso/thymleafy/web/controller/HomeController.java
@@ -2,14 +2,14 @@ package com.flc.curso.thymleafy.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/home")
 public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
+	
+	
 }
