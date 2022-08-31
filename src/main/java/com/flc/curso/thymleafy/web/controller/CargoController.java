ff86c57ca129b7adb18f355a69829f4250a82bbd
fernando.correa <paracorrea@gmail.com>
2022-08-31 10:50:43 -0300

    adiconando funcionalidades do departamento controller

--- src/main/java/com/flc/curso/thymleafy/web/controller/CargoController.java
+++ src/main/java/com/flc/curso/thymleafy/web/controller/CargoController.java
@@ -1,22 +1,41 @@
 package com.flc.curso.thymleafy.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.flc.curso.thymleafy.domain.Cargo;
+import com.flc.curso.thymleafy.service.CargoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	@Autowired
+	private CargoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		
+		model.addAttribute("cargo",service.buscarTodos() );
 		return "/cargo/lista";
+	}	
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo) {
+		service.salvar(cargo);
+		return "redirect:/cargo/cadastrar";
 	}
+		
+
 	
 	
 }
--- src/main/java/com/flc/curso/thymleafy/web/controller/DepartamentoController.java
+++ src/main/java/com/flc/curso/thymleafy/web/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,4 +35,15 @@ public class DepartamentoController {
 		return "redirect:/departamentos/cadastrar"; // return from method "/cadastrar" linha 20
 	}
 	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		service.editar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
 }
