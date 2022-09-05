af54ed98e5e94fd5f0e4aa65f1e89be1de88c7f4
fernando.correa <paracorrea@gmail.com>
2022-09-05 15:35:58 -0300

    funcionario editar e excluir em andamento

--- src/main/java/com/flc/curso/thymleafy/web/controller/FuncionarioController.java
+++ src/main/java/com/flc/curso/thymleafy/web/controller/FuncionarioController.java
@@ -22,66 +22,60 @@ import com.flc.curso.thymleafy.service.FuncionarioService;
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
-	
 	@Autowired
 	private FuncionarioService funcionarioService;
-	
+
 	@Autowired
 	private CargoService cargoService;
-	
-	
+
 	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
-		
-		//model.addAttribute("funcionarios",funcionarioService.buscarTodos() );
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
 		return "/funcionario/lista";
-	}	
-	
+	}
+
 	@PostMapping("/salvar")
 	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionario cadastrado com sucesso");
 		return "redirect:/funcionarios/cadastrar";
 	}
-		
 
-	
-	/*
-	 * @GetMapping("/editar/{id}") public String preEditar(@PathVariable("id") Long
-	 * id, ModelMap model) { model.addAttribute("cargo",
-	 * funcionarioService.buscarPorId(id)); return "/funcionario/cadastro"; }
-	 * 
-	 * @PostMapping("/editar") public String editar(Funcionario funcionario,
-	 * RedirectAttributes attr) { funcionarioService.editar(funcionario);
-	 * attr.addFlashAttribute("success", "Funcionario editado com sucesso"); return
-	 * "redirect:/funcionarios/cadastrar"; }
-	 */
-	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", funcionarioService.buscarPorId(id));
+		return "/funcionario/cadastro";
+	}
+
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionario editado com sucesso");
+		return "redirect:/funcionarios/cadastrar";
+	}
+
 	@ModelAttribute("cargos")
 	public List<Cargo> getCargos() {
 		return cargoService.buscarTodos();
 	}
-	
+
 	@ModelAttribute("ufs")
 	public UF[] getUFs() {
-			return UF.values();
-			
+		return UF.values();
+
 	}
-	/*
-	 * @GetMapping("/excluir/{id}") public String excluir(@PathVariable("id") Long
-	 * id, ModelMap model) {
-	 * 
-	 * if (funcionarioService.funcionarioTemCargo(id)) { model.addAttribute("fail",
-	 * "Departamento não removido. Possui funcionario(s) vinculado(s)"); } else {
-	 * funcionarioService.excluir(id); model.addAttribute("success",
-	 * "Funcionario excluido com sucesso"); }
-	 * 
-	 * return "redirect:/funcionarios/listar"; }
-	 */
-	
+
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		funcionarioService.excluir(id);
+		attr.addFlashAttribute("success", "Funcionario excluido com sucesso");
+		return "redirect:/funcionarios/listar";
+
+	}
+
 }
