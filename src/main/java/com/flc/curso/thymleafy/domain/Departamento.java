733678a11496dcdc87b87329943330c876232d7f
fernando.correa <paracorrea@gmail.com>
2022-09-06 11:55:12 -0300

    iniciado validacoes

--- src/main/java/com/flc/curso/thymleafy/domain/Departamento.java
+++ src/main/java/com/flc/curso/thymleafy/domain/Departamento.java
@@ -6,12 +6,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "DEPARTAMENTOS")
 public class Departamento extends AbstractEntity<Long> {
 	
+	@NotBlank(message = "informe um nome")
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;
 	
