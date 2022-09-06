c3b01db06999dc96ece10d2fa86c25671a870d41
fernando.correa <paracorrea@gmail.com>
2022-09-06 15:22:35 -0300

    iniciando validacao das classes endereco e funcionario

--- src/main/java/com/flc/curso/thymleafy/domain/Departamento.java
+++ src/main/java/com/flc/curso/thymleafy/domain/Departamento.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotBlank;
 public class Departamento extends AbstractEntity<Long> {
 	
 	@NotBlank(message = "informe um nome")
+	@Size(min = 3, max = 60, message = "O nome do departamento deve ter entre {min} e {max} caracteres.")
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;
 	
